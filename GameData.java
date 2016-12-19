78d34f79aca1b47756c216a1953e4eb772cb6774
jdlives <jlaborada.97@gmail.com>
2016-12-19 13:54:31 +0800

    added GameData class

---
+++ GameData.java
@@ -0,0 +1,24 @@
+public class GameData {
+    public static String[] wordDisplay = {"","","","","22"};
+    public static String[] systemWords = {"hello"
+                                          ,"hey"
+                                          ,"noscrub"
+                                          ,"ivy"
+                                          ,"daniel"
+                                          ,"laborada"
+                                          ,"jenn"
+                                          ,"codia"
+                                          ,"trish"
+                                          ,"evangelista"
+                                          ,"buy"
+                                          ,"my"
+                                          ,"ps4"
+                                          ,"maam"
+                                          ,"ivy"
+                                          ,"motherfather"
+                                          ,"merry"
+                                          ,"christmas"
+                                          ,"deadline"};
+
+
+}
